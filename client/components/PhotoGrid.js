530084335b99a11aa3773ecf5214b1b727f88ad0
Richard Moss <richard@englishtutorslive.com>
2017-02-11 22:31:44 +0000

    Adds base components and react router

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { link } from 'react-router';
+
+const PhotoGrid = React.createClass({
+  render() {
+    return (
+      <div className="photo-grid">
+        I'm the photo-grid
+      </div>
+    );
+  }
+});
+
+export default PhotoGrid;
---
+++ client/components/Single.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { link } from 'react-router';
+
+const Single = React.createClass({
+  render() {
+    return (
+      <div className="single-photo">
+        I'm the single-photo
+      </div>
+    );
+  }
+});
+
+export default Single;
